5ca4bc57ecc96b8917f73e536b66efe448850cfb
camilopg <53465674+camilopgonzalez@users.noreply.github.com>
2021-03-11 01:05:20 +0100

    Update ProductControllerTest.php

--- app/tests/Controller/ProductControllerTest.php
+++ app/tests/Controller/ProductControllerTest.php
@@ -7,6 +7,16 @@ use App\Entity\Product;
 
 class ProductControllerTest extends WebTestCase
 {
+
+    public function testIndexProduct()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $this->assertStringContainsString(
+            'Caja',
+            $client->getResponse()->getContent()
+        );
+    }
     public function testNewProduct()
     {
         $product = new Product();
@@ -18,5 +28,31 @@ class ProductControllerTest extends WebTestCase
 
         $client->request('POST', '/new', $array);
         $this->assertResponseIsSuccessful();
+    }
+
+    public function testShowProduct()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/1');
+        $this->assertStringContainsString(
+            'Caja',
+            $client->getResponse()->getContent()
+        );
+    }
+
+    public function testEditProduct()
+    {
+        $product = new Product();
+        $product->setName("Cajas");
+        $product->setDescription("Cajas de Madera");
+        $product->setPrice(30);
+        $array = (array) $product;
+        $client = static::createClient();
+        $client->request('POST', '/1/edit', $array);
+        $client->request('GET', '/');
+        $this->assertStringContainsString(
+            'Cajas',
+            $client->getResponse()->getContent()
+        );
     }
 }
